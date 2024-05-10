f26180be2feed190cfa1af4ec8f9429a9ae93376
Rod Sot <hi@rodsot.com>
2024-05-10 13:30:08 +1200

    config: add base configuration

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), mdx(), react()],
+  vite: {
+    css: {
+      devSourcemap: true
+    }
+  }
+});
